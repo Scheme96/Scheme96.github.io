25b34885ccf35049f35a4f890a17741f29e33dfa
Scheme96 <108583763+Scheme96@users.noreply.github.com>
2022-10-10 00:18:28 -0400

    123

--- js/button.js
+++
@@ -1,6 +0,0 @@
-var space = '&nbsp;';
-var button = document.createElement("button");
-mytext = "A" + space + space + space + space + "A";
-button.innerHTML = mytext;
-document.getElementById("buttonarea").appendChild(button);
-document.getElementById("textarea").innerHTML = mytext;
